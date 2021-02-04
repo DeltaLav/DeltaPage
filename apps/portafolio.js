3316fe39e30975e3e755e0cdb8e59a2388ada0c3
Carlos Lavieri <carlos_lavieri@pm.me>
2021-02-04 13:40:33 -0400

    Add "Portafolio"
    Se edito el html "portafolio", se agregaron logos mas datos en json

--- apps/portafolio.js
+++ apps/portafolio.js
@@ -15,6 +15,38 @@ function traerDatos(){
             let datos = JSON.parse(this.responseText);
 
             console.log(datos);
+            
+            let pages = document.querySelector('#pages');
+            pages.innerHTML = '';
+
+            let i = 0;
+            for(let item of datos){
+                pages.innerHTML += `
+                    <div class="col-sm">
+                        <div class="card bg-secondary">
+                            <div class="card-header">
+                                <h3 class="card-title">${item.name}</h3>
+                            </div>
+                            <div class="card-body">
+                                <img src="../img/logo-pages/${item.name}.png" class="rounded-circle" height="100px" alt="${item.name}" title="${item.name}">
+                                <h4 class="card-subtitle">${item.type}</h4>
+                                <p class="card-text" id="keys${i}"></p>
+                            </div>
+                            <div class="card-footer">
+                                <div class="btn-group">
+                                    <a href="${item.url}" class="btn btn-success ">Ir a Página</a>    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                let key = document.querySelector('#keys'+i);
+                key.innerHTML = '';
+                for(j of item.keys){
+                    key.innerHTML += `| ${j} | `; 
+                }
+                i++;
+            }
         }
     }
 }
--- apps/sobre.js
+++ apps/sobre.js
@@ -21,17 +21,106 @@ function traerDatos(){
 
             about.innerHTML = `<p>Soy <strong>${datos.name} ${datos.lastName}</strong> mejor conocido como <b>"Lav"</b>, un joven que quiere aprender a programar y desarrollar paginas web. En estos momentos me encuentro aprendiendo en <strong><a class="badge bg-success" href="https://www.sololearn.com/">SoloLearn</a></strong> lo básico para alcanzar lo que quiero.</p>`
 
-            var i = 0, j = 1;
+            var i = 0;
             for(let item of datos.tecnologias){
+                // if(i == 0){
+                //     if(item.certificate == true){
+                //         hc.innerHTML += `
+                //         <div class="row">
+                //             <div class="col-sm">
+                //                 <div class="card bg-secondary">
+                //                     <div class="card-body">
+                //                         <h6 class="card-title">${item.name} <svg xmlns="http://www.w3.org/2000/svg" alt="Certificate" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                //                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                //                             </svg> > <span class="badge bg-success">${item.level}</span></h6>
+                //                         <p class="card-text">${item.format}</p>
+                //                     </div>
+                //                 </div>
+                //             </div>
+                //         `;
+                //     }
+                //     else{
+                //         hc.innerHTML += `
+                //         <div class="row">
+                //             <div class="col-sm">
+                //                 <div class="card bg-secondary">
+                //                     <div class="card-body">
+                //                         <h6 class="card-title">${item.name} > <span class="badge bg-success">${item.level}</span></h6>
+                //                         <p class="card-text">${item.format}</p>
+                //                     </div>
+                //                 </div>
+                //             </div>
+                //         `;
+                //     }
+                // }
+                // if(i == 2){
+                //     if(item.certificate == true){
+                //         hc.innerHTML += `
+                //             <div class="col-sm">
+                //                 <div class="card bg-secondary">
+                //                     <div class="card-body">
+                //                         <h6 class="card-title">${item.name} <svg xmlns="http://www.w3.org/2000/svg" alt="Certificate" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                //                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                //                             </svg> > <span class="badge bg-success">${item.level}</span></h6>
+                //                         <p class="card-text">${item.format}</p>
+                //                     </div>
+                //                 </div>
+                //             </div>
+                //         </div>
+                //         `;
+                //     }
+                //     else{
+                //         hc.innerHTML += `
+                //             <div class="col-sm">
+                //                 <div class="card bg-secondary">
+                //                     <div class="card-body">
+                //                         <h6 class="card-title">${item.name} > <span class="badge bg-success">${item.level}</span></h6>
+                //                         <p class="card-text">${item.format}</p>
+                //                     </div>
+                //                 </div>
+                //             </div>
+                //         </div>
+                //         `;
+                //     }
+                //     i = 0;
+                // }
+                // else{
+                //     if(item-certificate == true){
+                //         hc.innerHTML += `
+                //             <div class="col-sm py-1">
+                //                 <div class="card bg-secondary">
+                //                     <div class="card-body">
+                //                         <h6 class="card-title">${item.name} <svg xmlns="http://www.w3.org/2000/svg" alt="Certificate" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                //                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                //                         </svg> > <span class="badge bg-success">${item.level}</span></h6>
+                //                         <p class="card-text">${item.format}</p>
+                //                     </div>
+                //                 </div>
+                //             </div>`;
+                //     }
+                //     else{
+                //         hc.innerHTML += `
+                //             <div class="col-sm py-1">
+                //                 <div class="card bg-secondary">
+                //                     <div class="card-body">
+                //                         <h6 class="card-title">${item.name} > <span class="badge bg-success">${item.level}</span></h6>
+                //                         <p class="card-text">${item.format}</p>
+                //                     </div>
+                //                 </div>
+                //             </div>`;
+                //     }
+                // }
                 hc.innerHTML += `
-                    <div class="col-sm py-1">
-                        <div class="card">
-                            <div class="card-body">
-                                <h6 class="card-title">${item.name} > <span class="badge bg-success">${item.level}</span></h6>
-                                <p class="card-text">${item.format}</p>
-                            </div>
-                        </div>
-                    </div>`;
+                                <div class="col-sm py-1">
+                                    <div class="card bg-secondary">
+                                        <div class="card-body">
+                                            <h6 class="card-title">${item.name} > <span class="badge bg-success">${item.level}</span></h6>
+                                            <p class="card-text">${item.format}</p>
+                                        </div>
+                                    </div>
+                                </div>`;
+                i++;
+                console.log(i);
             }
 
             console.log(datos);
